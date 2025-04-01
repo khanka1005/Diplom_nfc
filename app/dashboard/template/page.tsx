ca94947a0f82167585184ba9cf7ccceb25d55e10
Khanka1005 <khanerdene2003@gmail.com>
2025-04-01 23:28:07 +0800

    Fix ESLint config and disable no-explicit-any

--- app/dashboard/template/page.tsx
+++ app/dashboard/template/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Template from "@/app/dashboard/template/template"; // Move Template to a separate component file
+import Template from "@/app/dashboard/components/template"; // Move Template to a separate component file
 
 const Page = () => {
   return <Template />;
