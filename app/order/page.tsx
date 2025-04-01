e8291e992f2c890391207b2a50c53fe0e0e2818c
Khanka1005 <khanerdene2003@gmail.com>
2025-04-02 00:16:58 +0800

    fix: vercel config to prevent npm auth error

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,13 +1,18 @@
 "use client";
-import ContentSlider from "../components/ContentSection/ContentSlider";
+
+import dynamic from "next/dynamic";
+
+// Dynamically import ContentSlider with SSR disabled
+const ContentSlider = dynamic(() => import("../components/ContentSection/ContentSlider"), {
+  ssr: false,
+});
 
 const Ordering = () => {
-    return ( 
-        <div className="flex flex-col  min-h-screen bg-gray-100">
-      
-        <ContentSlider />
-      </div>
-     );
-}
- 
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <ContentSlider />
+    </div>
+  );
+};
+
 export default Ordering;
