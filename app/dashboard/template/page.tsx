8c5840f73b946c5cb5e3937b7c70b62b982ec765
Khanka1005 <khanerdene2003@gmail.com>
2025-04-01 23:58:51 +0800

    Fix ESLint config and disable no-explicit-any

--- app/dashboard/template/page.tsx
+++ app/dashboard/template/page.tsx
@@ -1,13 +1,12 @@
 // app/dashboard/template/page.tsx
-"use client"
+'use client'
 import dynamic from 'next/dynamic';
-import React from 'react';
+
 
 // Dynamically import the Template component with SSR disabled
-const Template = dynamic(() => import('./template'), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-});
+const Template = dynamic(() => import('./template'), 
+  { ssr: false }
+);
 
 const Page = () => {
   return <Template />;
